27efb80a349df7ef26ff527d325b5e8a25562b1d
Krishna-B5 <krishnab.05@gmail.com>
2026-01-23 17:20:44 +0530

    infer_schema prgm

--- src/main/scala/infer_schema.scala
+++ src/main/scala/infer_schema.scala
@@ -1,7 +1,27 @@
+
+import org.apache.spark.sql.SparkSession
+
+
 object infer_schema {
 
   def main(args:Array[String]):Unit ={
 
-    print(" This prg about InferSchema")
+    print(" This prg explains InferSchema")
+
+    val spark = SparkSession.builder()  //  creating a SparkSession
+      .appName("InferSchema")
+      .master("local[*]")
+      .getOrCreate()
+
+    val df = spark.read   //  Reading a CSV file
+      .format("CSV")
+      .option("header",true)
+      .option("inferschema",true)
+      .option("path","C:/Users/91973/Desktop/Practise_files/emp.csv")
+      .load()
+
+    df.show()   //  displaying the data
+
+    df.printSchema()     //  printing schema
   }
 }
